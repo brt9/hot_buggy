391630348a25c627a87bfa00d5142566a9d9ef08
brt9 <50088462+brt9@users.noreply.github.com>
2023-09-28 23:02:27 -0300

    navbar_update

---
+++ turismo/resources/views/includes/navbar.blade.php
@@ -0,0 +1,88 @@
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="z-index: 2; position: relative;">
+    <div class="container px-4 px-lg-5">
+        <a class="navbar-brand" href="#!" style="color: #25D366;"><b>Buggy 3D</b></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="/"><b>Inicio</b></a>
+                </li>
+                <li class="nav-item"><a class="nav-link active" href="/sobre"><b>Sobre Nós</b></a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" id="navbarDropdown" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false"><b>Passeios</b></a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/passeios?id=1">Buggy Litoral Norte (Genipabu)</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=2">Praia de Pipa</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=3">Litoral Sul Vip – Off road 4×4</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=4">Maracajaú + Mergulho (Catamarã)</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=5">Maracajaú + Mergulho (Lancha)</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=6">Perobas + Punaú</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=7">Galinhos com Barco</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=8">São Miguel do Gostoso</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=9">Barra do Cunhaú</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=10">João Pessoa / City-tour + Pôr do Sol</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=11">Barra do Cunhaú</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=12">Buggy Litoral Sul (Pipa)</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=13">Litoral Sul com Lagoa de Arituba</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=14">Praia de Maracajaú (sem mergulho)</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="/passeios?id=15">Forró com Turista</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link active" href="/galeria"><b>Galeria</b></a></li>
+                <li class="nav-item"><a class="nav-link active" href="/contato"><b>Contato</b></a></li>
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-outline-dark" type="submit">
+                    <i class="bi-cart-fill me-1"></i>
+                    Carrinho
+                    <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                </button>
+            </form>
+        </div>
+    </div>
+</nav>
--- turismo/resources/views/welcome.blade.php
+++ turismo/resources/views/welcome.blade.php
@@ -16,173 +16,86 @@
 </head>
 
 <body>
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="z-index: 2; position: relative;">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!" style="color: #25D366;"><b>Buggy 3D</b></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!"><b>Inicio</b></a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link active" href="#!"><b>Sobre Nós</b></a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" id="navbarDropdown" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false"><b>Passeios</b></a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">Buggy Litoral Norte (Genipabu)</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Praia de Pipa</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Litoral Sul Vip – Off road 4×4</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Maracajaú + Mergulho (Catamarã)</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Maracajaú + Mergulho (Lancha)</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Perobas + Punaú</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Galinhos com Barco</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">São Miguel do Gostoso</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Barra do Cunhaú</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">João Pessoa / City-tour + Pôr do Sol</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Barra do Cunhaú</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Buggy Litoral Sul (Pipa)</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Litoral Sul com Lagoa de Arituba</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Praia de Maracajaú (sem mergulho)</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Forró com Turista</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item"><a class="nav-link active" href="#!"><b>Galeria</b></a></li>
-                    <li class="nav-item"><a class="nav-link active" href="#!"><b>Contato</b></a></li>
-                </ul>
-                <form class="d-flex">
-                    <button class="btn btn-outline-dark" type="submit">
-                        <i class="bi-cart-fill me-1"></i>
-                        Carrinho
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                    </button>
-                </form>
+    @include('includes.navbar') 
+<!-- Header-->
+<div class="content" style="z-index: 1; position: relative;">
+    <header class="bg-dark" style="padding-top: 150px; padding-bottom: 100px;">
+        <video autoplay muted loop poster="imagem-de-fallback.jpg" preload="auto">>
+            <source src="assets/buggy.mp4" type="video/mp4">
+            <!-- Adicione mais formatos de vídeo, se necessário -->
+            Seu navegador não suporta a reprodução de vídeo.
+        </video>
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white header-content">
+                <h1 class="display-4 fw-bolder">Buggy 3D Natal</h1>
+                <h2 class="lead fw-normal mb-0">Destinos
+                    Descoberta
+                    Diversão</h2>
             </div>
         </div>
-    </nav>
-    <!-- Header-->
-    <div class="content" style="z-index: 1; position: relative;">
-        <header class="bg-dark" style="padding-top: 150px; padding-bottom: 100px;">
-            <video autoplay muted loop poster="imagem-de-fallback.jpg" preload="auto">>
-                <source src="assets/buggy.mp4" type="video/mp4">
-                <!-- Adicione mais formatos de vídeo, se necessário -->
-                Seu navegador não suporta a reprodução de vídeo.
-            </video>
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white header-content">
-                    <h1 class="display-4 fw-bolder">Buggy 3D Natal</h1>
-                    <h2 class="lead fw-normal mb-0">Destinos
-                        Descoberta
-                        Diversão</h2>
-                </div>
-            </div>
-        </header>
+    </header>
 
-        <!-- Section-->
-        <section class="py-5">
-            <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    @foreach ($passeios as $passeio)
-                        <div class="col mb-5">
-                            <div class="card h-100">
-                                <!-- Product image-->
-                                <img class="card-img-top" src="{{ $passeio->imagem }}" alt="..." />
-                                <!-- Product details-->
-                                <div class="card-body p-4">
-                                    <div class="text-center">
-                                        <!-- Product name-->
-                                        <h5 class="fw-bolder">
-                                            <h2>{{ $passeio->nome }}</h2>
-                                        </h5>
-                                        <!-- Product price-->
-                                        R$ {{ number_format($passeio->preco, 2) }}
-                                        <!-- Product description-->
-                                        <p>
-                                            {{ substr($passeio->descricao, 0, 100) }}
-                                            <!-- Mostra os primeiros 100 caracteres da descrição -->
-                                            @if (strlen($passeio->descricao) > 100)
-                                                ...
-                                                <!-- Adiciona reticências se a descrição for maior que 100 caracteres -->
-                                            @endif
-                                        </p>
-                                    </div>
+    <!-- Section-->
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                @foreach ($passeios as $passeio)
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="{{ $passeio->imagem }}" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">
+                                        <h2>{{ $passeio->nome }}</h2>
+                                    </h5>
+                                    <!-- Product price-->
+                                    R$ {{ number_format($passeio->preco, 2) }}
+                                    <!-- Product description-->
+                                    <p>
+                                        {{ substr($passeio->descricao, 0, 100) }}
+                                        <!-- Mostra os primeiros 100 caracteres da descrição -->
+                                        @if (strlen($passeio->descricao) > 100)
+                                            ...
+                                            <!-- Adiciona reticências se a descrição for maior que 100 caracteres -->
+                                        @endif
+                                    </p>
                                 </div>
-                                <!-- Product actions-->
-                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center">
-                                        <a class="btn btn-outline-dark mt-auto" href="/passeios?id={{ $passeio->id }}">
-                                            Saiba Mais
-                                        </a>
-                                    </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center">
+                                    <a class="btn btn-outline-dark mt-auto" href="/passeios?id={{ $passeio->id }}">
+                                        Saiba Mais
+                                    </a>
                                 </div>
                             </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             </div>
-        </section>
-        
-        
-        <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container">
-                <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
-            </div>
-        </footer>
+        </div>
+    </section>
 
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <div class="w3-whatsapp font0"><a href="https://api.whatsapp.com/send?phone=5584998102246"
-                class="flex justify-center align-center" target="_blank"><img
-                    src="https://images.tcdn.com.br/706061/themes/55/img/logo_whatsapp.svg?dfd189f6cded1031d1dad5c3ac67474e"
-                    alt="Logo WhatsApp"></a></div>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-    </div>
+
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="w3-whatsapp font0"><a href="https://api.whatsapp.com/send?phone=5584998102246"
+            class="flex justify-center align-center" target="_blank"><img
+                src="https://images.tcdn.com.br/706061/themes/55/img/logo_whatsapp.svg?dfd189f6cded1031d1dad5c3ac67474e"
+                alt="Logo WhatsApp"></a></div>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</div>
 </body>
 
 </html>
