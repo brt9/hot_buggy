f54343919c6df7bd4a66f9418884caf558edd583
brt9 <50088462+brt9@users.noreply.github.com>
2024-04-21 02:29:10 -0300

    update_navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -9,92 +9,101 @@
     <title>Hot Buggy</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Bootstrap icons-->
+  <link rel="icon" type="image/x-icon" href="assets/image.png" style="background-color: white;" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic"
+        rel="stylesheet" type="text/css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+    <link href="css/animate.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/icomoon.css" rel="stylesheet" />
+    <link href="css/flaticon.css" rel="stylesheet" />
 </head>
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="z-index: 2; position: relative;">
-    <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#!" style="color: #000;">
-            <span style="color: #ff2800;"><b>Hot</b></span> Buggy
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
-                class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="/"><b>Inicio</b></a>
-                </li>
-                <li class="nav-item"><a class="nav-link active" href="/sobre"><b>Sobre Nós</b></a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle active" id="navbarDropdown" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false"><b>Passeios</b></a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/passeios?id=1">Buggy Litoral Norte (Genipabu)</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=2">Praia de Pipa</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=3">Litoral Sul Vip – Off road 4×4</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=4">Maracajaú + Mergulho (Catamarã)</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=5">Maracajaú + Mergulho (Lancha)</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=6">Perobas + Punaú</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=7">Galinhos com Barco</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=8">São Miguel do Gostoso</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=9">Barra do Cunhaú</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=10">João Pessoa / City-tour + Pôr do Sol</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=11">Barra do Cunhaú</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=12">Buggy Litoral Sul (Pipa)</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=13">Litoral Sul com Lagoa de Arituba</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=14">Praia de Maracajaú (sem mergulho)</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="/passeios?id=15">Forró com Turista</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item"><a class="nav-link active" href="/galeria"><b>Galeria</b></a></li>
-                <li class="nav-item"><a class="nav-link active" href="/contato"><b>Contato</b></a></li>
-            </ul>
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#!" style="color: #000;">
+                <span style="color: #ff2800;"><b>Hot</b></span> Buggy
+            </a>
+        <!--    <img src="assets/image.png" href="#page-top" alt="" srcset="" style="width: 80px; height: 50px;"> -->
+            <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="navbar-collapse collapse" id="navbarResponsive" style="">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="/"><b>Inicio</b></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link active" href="/sobre"><b>Sobre Nós</b></a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" id="navbarDropdown" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"><b>Passeios</b></a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/passeios?id=1">Buggy Litoral Norte (Genipabu)</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=2">Praia de Pipa</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=3">Litoral Sul Vip – Off road 4×4</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=4">Maracajaú + Mergulho (Catamarã)</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=5">Maracajaú + Mergulho (Lancha)</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=6">Perobas + Punaú</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=7">Galinhos com Barco</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=8">São Miguel do Gostoso</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=9">Barra do Cunhaú</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=10">João Pessoa / City-tour + Pôr do Sol</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=11">Barra do Cunhaú</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=12">Buggy Litoral Sul (Pipa)</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=13">Litoral Sul com Lagoa de Arituba</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=14">Praia de Maracajaú (sem mergulho)</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li><a class="dropdown-item" href="/passeios?id=15">Forró com Turista</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link active" href="/galeria"><b>Galeria</b></a></li>
+                    <li class="nav-item"><a class="nav-link active" href="/contato"><b>Contato</b></a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+
